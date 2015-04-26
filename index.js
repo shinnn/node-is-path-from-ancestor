e29b6ae31d5820811a20a03cf59c7046239a70f5
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-04-26 10:12:56 +0900

    improve error message

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
  * is-path-from-ancestor | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/node-is-path-from-ancestor
 */
-
 'use strict';
 
 var path = require('path');
 
 module.exports = function isPathFromAncestor(str) {
   if (typeof str !== 'string') {
-    throw new TypeError(str + ' is not a string.');
+    throw new TypeError(
+      str +
+      ' is not a string. The first argument to is-path-from-ancestor must be a file path.'
+    );
   }
 
   return path.normalize(str).indexOf('..' + path.sep) === 0;
--- test.js
+++ test.js
@@ -2,29 +2,35 @@
 
 var sep = require('path').sep;
 
-var isPathFromAncestor = require('require-main')();
+var isPathFromAncestor = require('./');
 var test = require('tape');
 
 test('isPathFromAncestor', function(t) {
-  t.plan(4);
+  t.plan(5);
 
-  t.ok(
-    isPathFromAncestor('..' + sep) === true,
+  t.equal(isPathFromAncestor.name, 'isPathFromAncestor', 'should have a function name.');
+
+  t.strictEqual(
+    isPathFromAncestor('..' + sep),
+    true,
     'should return true when the path starts with ancestor reference.'
   );
 
-  t.ok(
-    isPathFromAncestor('..') === false,
+  t.strictEqual(
+    isPathFromAncestor('..'),
+    false,
     'should return false when the path doesn\'t start with ancestor reference.'
   );
 
-  t.ok(
-    isPathFromAncestor('foo' + sep + '..' + sep) === false,
+  t.strictEqual(
+    isPathFromAncestor('foo' + sep + '..' + sep),
+    false,
     'should normalize the path before checking.'
   );
 
   t.throws(
-    isPathFromAncestor.bind(null, ['foo']), /is not a string/,
-    'should throw a type error when the argument is not a string.'
+    isPathFromAncestor.bind(null, ['foo']),
+    /TypeError.* is not a string/,
+    'should throw a type error when it takes a non-string argument.'
   );
 });
